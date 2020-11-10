19b2d3b987bfa8ccdd9aeb04e66f79f2126cf271
JanuszPXYZ <19962689+JanuszPXYZ@users.noreply.github.com>
2020-11-11 00:39:37 +0100

    Closures

--- Unit 5/Notes/Closures.playground/Contents.swift
+++ Unit 5/Notes/Closures.playground/Contents.swift
@@ -94,3 +94,96 @@ let evenMoreSugary = tracks.sorted { return $0.trackName < $1.trackName}
 
 // When a closure has one line, Swift will automatically return the result, so you can remove the return keyword.
 
+let totalSugar = tracks.sorted { $0.trackName < $1.trackName }
+
+// TRAILING CLOSURE SYNTAX
+
+func performRequest(url: String, response: (_ code: Int) -> Void) {
+}
+
+// Here's how we would call the function above
+performRequest(url: "https://www.apple.com") { (data) in
+    print(data)
+}
+
+
+// COLLECTION FUNCTIONS USING CLOSURES
+// Swift includes some advanced functions for iterating over collections and performing common actions. We'll learn about map, which allows us to transform an array of one type into an array of another type by transforming each value in the collection. We'll learn about filter, which creates a new array of only the objects that meet our defined set of rules. And we'll also learn about reduce, which lets us combine many values into a single one.
+
+// For starters, M A P function
+
+let names: [String] = ["Johnny", "Nellie", "Aaron", "Rachel"]
+var fullNames: [String] = []
+
+for name in names {
+    let full = name + " Smith"
+    fullNames.append(full)
+}
+print(fullNames)
+// We could of course use a for loop to create an array of full names, but there's a more clever way: the map function. The closure will be executed on each object in the firstNames array to create what will be added to the fullNames array
+let fullNamesClosure = names.map { (name) -> String in
+    return name + " Smith"
+}
+print(fullNamesClosure)
+
+// A shortened version of the map function works as well. Recall that $0 is just a placeholder for the current object in the array
+
+let fullNamesSugar = names.map { $0 + " Smith" }
+fullNamesSugar
+
+// Closure syntax can be difficult. It's helpful to pause and break down the syntax of each example. Try to identify the:
+// 1. function call -> map()
+// 2. closure parameter -> The code in the curly braces {}
+// 3. $0 argument -> The individual object in the array that's being worked with.
+
+// FILTER function
+
+let numbers: [Int] = [4, 8, 15, 16, 23, 42]
+var numbersLessThan20: [Int] = []
+
+for number in numbers {
+    if number < 20 {
+        numbersLessThan20.append(number)
+    }
+}
+
+// And here's the filter function (two versions of it: one being more explicit, and the other succinct)
+let explicitNums = numbers.filter { (number) -> Bool in
+    number < 20
+}
+
+let filteredNums = numbers.filter { $0 < 20 }
+
+
+// REDUCE function
+// The reduce function combines all of the values in an array into one value. It takes a starting value and a closure that dictates how to combine the items. The code in this example takes an array of numbers and adds them together.
+
+// Here's one potential solution
+
+let anotherSetOfNumbers: [Int] = [8, 6, 7, 5, 3, 0, 9]
+var total: Int = 0
+
+for number in anotherSetOfNumbers {
+    total += number
+}
+print(total)
+
+// And another solution using the reduce function
+
+let reduceTotal = anotherSetOfNumbers.reduce(0) { (currentTotal, newValue) -> Int in
+    return currentTotal + newValue
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
